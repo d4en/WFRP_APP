6399b4d2d4b5e4cda45ca19732bfff02fc27d17e
D4en <karol.majstrowicz@gmail.com>
2013-10-26 15:16:38 +0200

    Working WCF Service for WFRP APP

---
+++ WFRP_Service/Service/WFRPService.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace Service
+{
+
+    [DataContract]
+    public class Client
+    {
+        private string _name;
+
+        [DataMember]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+    }
+
+    [DataContract]
+    public class Message
+    {
+        private string _sender;
+        private string _content;
+
+        [DataMember]
+        public string Sender
+        {
+            get { return _sender; }
+            set { _sender = value; }
+        }
+        [DataMember]
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+    }
+
+
+    [ServiceContract(CallbackContract = typeof(IWFRPCallback), SessionMode = SessionMode.Required)]
+    public interface IWFRP
+    {
+        [OperationContract(IsInitiating = true)]
+        bool Connect(Client client);
+
+    }
+
+    public interface IWFRPCallback
+    {
+
+        [OperationContract(IsOneWay = true)]
+        void Receive(Message msg);
+
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single,
+        ConcurrencyMode = ConcurrencyMode.Multiple,
+        UseSynchronizationContext = false)]
+    public class WFRPService : IWFRP
+    {
+        Dictionary<Client, IWFRPCallback> clients =
+                 new Dictionary<Client, IWFRPCallback>();
+
+        List<Client> clientList = new List<Client>();
+
+        public IWFRPCallback CurrentCallback
+        {
+            get
+            {
+                return OperationContext.Current.
+                       GetCallbackChannel<IWFRPCallback>();
+            }
+        }
+
+        object syncObj = new object();
+
+        private bool SearchClientsByName(string name)
+        {
+            foreach (Client c in clients.Keys)
+            {
+                if (c.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #region IWFRP Members
+
+        public bool Connect(Client client)
+        {
+            if (!clients.ContainsValue(CurrentCallback) &&
+                !SearchClientsByName(client.Name))
+            {
+                lock (syncObj)
+                {
+                    clients.Add(client, CurrentCallback);
+                    clientList.Add(client);
+
+                    foreach (Client key in clients.Keys)
+                    {
+                        IWFRPCallback callback = clients[key];
+                        try
+                        {
+
+                            Message msg = new Message();
+                            msg.Sender = "Server";
+                            msg.Content = "test";
+                            callback.Receive(msg);
+                        }
+                        catch
+                        {
+                            clients.Remove(key);
+                            return false;
+                        }
+
+                    }
+
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+        
+
+        #endregion
+    }
+
+}
