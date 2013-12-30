976251253324f774cdac8f7acaad428f161244dd
ewciks <ewa.sobieniak@gmail.com>
2013-12-31 00:11:56 +0100

    Client - GUI reworks:
    - all windows use MetroWindowStyle (window title and close, min, max buttons)
    - all buttons use MetroButtonStyle

--- WFRP_Service/WPFClient/OptionsWindow.xaml.cs
+++ WFRP_Service/WPFClient/OptionsWindow.xaml.cs
@@ -109,11 +109,38 @@ namespace WPFClient
             servCom.ViewHero();
             heroWindow.Show();
         }
-
+        
         #endregion
 
-        
+        #region MetroStyle window bar
 
-        
+        private void PART_TITLEBAR_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void PART_CLOSE_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PART_MAXIMIZE_RESTORE_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == System.Windows.WindowState.Normal)
+            {
+                this.WindowState = System.Windows.WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = System.Windows.WindowState.Normal;
+            }
+        }
+
+        private void PART_MINIMIZE_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        #endregion
     }
 }
